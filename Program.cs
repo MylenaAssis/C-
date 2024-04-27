8710a3b63a9596bc039ea2f405451ee413b5bc04
Mylena Assis <lena.assis.dev@gmail.com>
2024-04-27 18:05:15 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+//variaveis declaradas em camelCase
+string boasVindas = "Seja bem vindo ao ScreenSound";
+
+//palavra reservada void para indicar funcao. Funcoes escritas em PascalCase
+void ExibirMensagemDeBoasVindas()
+{
+    //uso de @ para mostrar a string como ela será apresentada no console, facilitando visualizacao
+    Console.WriteLine(@"
+        
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+    Console.WriteLine(boasVindas);
+}
+
+ExibirMensagemDeBoasVindas();
